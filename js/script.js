45b1b4224414d9306088c85e875699b0b9d33a19
palentt <clusselchen@gmail.com>
2024-11-07 00:57:55 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ hitungBtn.addEventListener("click", function () {
   let bb = document.getElementById("bb").value;
   let tb = document.getElementById("tb").value;
   const deskripsi = document.getElementById("deskripsi");
+  const penjelasan = document.getElementById("penjelasan");
   let kotak = document.getElementById("kotak-hasil");
 
   //perhitungan
@@ -50,15 +51,33 @@ hitungBtn.addEventListener("click", function () {
   if (result < 18.5) {
     deskripsi.innerHTML =
       "Kamu <span style='color: rgb(228, 198, 0); font-weight: bold'>Kekurangan</span>  berat badan ";
+    kotak.classList.add("bg-kuning");
   } else if (result >= 18.5 && result <= 24.9) {
     deskripsi.innerHTML =
       "Berat badan kamu <span style='color: rgb(28, 205, 146); font-weight: bold'>Ideal</span> ";
+    kotak.classList.add("bg-ijo");
   } else if (result >= 25 && result <= 29.9) {
     deskripsi.innerHTML =
       "Berat badan kamu <span style='color: rgb(255, 140, 0); font-weight: bold'>Berlebih</span> ";
+    kotak.classList.add("bg-oren");
   } else if (result > 30) {
     deskripsi.innerHTML =
       "Berat badan kamu <span style='color: red ; font-weight: bold'>Obesitas</span> ";
+    kotak.classList.add("bg-merah");
+  }
+  //penjelasan tetang Hasil
+  if (result < 18.5) {
+    penjelasan.innerHTML =
+      "Badan kamu kekurangan Berat Badan, makan lah lebih banyak dan bergizi ";
+  } else if (result >= 18.5 && result <= 24.9) {
+    penjelasan.innerHTML =
+      "Selamat Berat badan kamu sudah ideal, jagalah pola makan  ";
+  } else if (result >= 25 && result <= 29.9) {
+    penjelasan.innerHTML =
+      "Berat Badan kamu sudah mulai berlebih sebaiknya mulailah untuk berolahraga dan mengurangi kalori harian</span> ";
+  } else if (result > 30) {
+    penjelasan.innerHTML =
+      "Berat Badan kamu sudah berlebihan segera untuk berolahraga dan mengurangi kalori harian ";
     console.log(kotak);
     kotak.classList.add("bg-merah");
   }
